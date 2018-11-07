42c7e0b1227734c06bd7dfdf4d3548ec0c00b643
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2018-11-07 14:40:31 +0800

    Initial commit

---
+++ src/main/java/com/xulei/kidhttpproxy/KidhttpproxyApplication.java
@@ -0,0 +1,14 @@
+package com.xulei.kidhttpproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class KidhttpproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidhttpproxyApplication.class, args);
+	}
+}
